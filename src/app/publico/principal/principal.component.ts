05862663799fe2bb5a91efcaa5de9f7d66fd1b1e
Lizbeth06 <lizbeth.infante.27@unsch.edu.pe>
2024-06-27 18:16:21 -0500

    segundo cambio

--- src/app/publico/principal/principal.component.ts
+++ src/app/publico/principal/principal.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+
+import {provideNativeDateAdapter} from '@angular/material/core';
+
 import { MatInputModule } from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -7,11 +10,14 @@ import {MatDividerModule} from '@angular/material/divider'
 import { MatMenuModule } from '@angular/material/menu';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CommonModule } from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [MatInputModule, MatToolbarModule, MatButtonModule, MatDividerModule, MatIconModule, MatMenuModule, FlexLayoutModule, CommonModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatInputModule, MatToolbarModule, MatButtonModule, MatDividerModule, MatIconModule, MatMenuModule, FlexLayoutModule, CommonModule, MatFormFieldModule, MatDatepickerModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
